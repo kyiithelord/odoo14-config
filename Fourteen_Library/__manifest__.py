1be55082d5323989c65df1739ad6659e0f0f5adc
kyiithelord <thetzinkyaw9727@gmail.com>
2024-10-14 10:50:59 +0630

    update

---
+++ Fourteen_Library/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "My library",
+    'summary': "Manage books easily",
+    'author': "Innovix",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'data': [
+        'views/library_book.xml',
+        'security/ir.model.access.csv'
+    ],
+}
