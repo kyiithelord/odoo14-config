d2bf5f96fcbfb2acf83f457f99f3a5858b454d5b
kyiithelord <thetzinkyaw9727@gmail.com>
2024-10-15 09:48:15 +0630

    hospital module conf

---
+++ Hospital_Library/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Hospital Management",
+    'category' : 'Extra Tools',
+    'summary': "Module for managing Hospitals",
+    'author': "Odoo Mate",
+    'maintainer':'Odoo Mate',
+    'website': "http://www.odoomate.com",
+    'data': [
+
+    ],
+}
